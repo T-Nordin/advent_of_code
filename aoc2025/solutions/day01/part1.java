4a7c10209ba24128828ec6285e8a92a7af0afbb6
T-Nordin <247227688+T-Nordin@users.noreply.github.com>
2025-12-01 22:34:45 +0100

    added complete solution for part1

--- aoc2025/solutions/day01/part1.java
+++ aoc2025/solutions/day01/part1.java
@@ -8,6 +8,38 @@ public class part1 {
     public static void main(String[] args) {
         List<String> input = InputReader.readLines("aoc2025/inputs/day01/inputPart1.txt");
 
-        System.out.println(input);
+        System.out.println(solve(input));
     }
+
+    private static int solve(List<String> input) {
+        int result = 0;
+        int position = 50;
+
+        for (String string : input) {
+            char direction = string.charAt(0);
+            int distance = Integer.parseInt(string.substring(1));
+            switch (direction) {
+                case 'R' -> position = finalizePosition(position += distance);
+                case 'L' -> position = finalizePosition(position -= distance);
+                default -> throw new IllegalArgumentException("Invalid direction: " + direction);
+            }
+            if (position == 0) {
+                result++;
+            }
+        }
+
+        return result;
+
+    }
+
+    private static int finalizePosition(int position) {
+        if(position < 0) {
+            return finalizePosition(position + 100);
+        } else if (position > 99) {
+            return finalizePosition(position - 100);
+        } else {
+            return position;
+        }
+    }
+
 }
